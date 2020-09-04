48de3ec4d256d551e8fa053fdb6f0594458d4320
PetePlays <peteplays@hotmail.com>
2020-09-04 12:24:22 -0400

    cleaned up `daysInMonths`

--- client/src/utils/months.ts
+++ client/src/utils/months.ts
@@ -1,10 +1,14 @@
+interface IDaysInMonths {
+  [k: number]: number;
+}
+
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export const daysInMonths = (selectedYear: number) =>
-  months.reduce((acc: any, _, i) => {
+  months.reduce((acc: IDaysInMonths, _, i) => {
     const date = new Date(selectedYear, i + 1, 0);
 
-    acc[i] = (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate())
+    acc[i] = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
 
     return acc;
   }, {});
